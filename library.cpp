9a63ac156c5434a08a65ea3d20d097e0e6c86fbc
Muyen Hasan <muyen1311@gmail.com>
2026-04-23 02:16:27 +0600

    added showing & issue function

--- library.cpp
+++ library.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <cstdio>
+#include <cstdio>   // for remove & rename
 using namespace std;
 
 // Add Book
@@ -19,4 +19,54 @@ void addBook() {
     file.close();
 
     cout << "Book Added Successfully!\n";
+}
+
+// Show All Books
+void showAll() {
+    string id, name, author, status;
+    ifstream file("bookData.txt");
+
+    cout << "\nID\tName\tAuthor\tStatus\n";
+    cout << "----------------------------------------\n";
+
+    while (getline(file, id, '*') &&
+           getline(file, name, '*') &&
+           getline(file, author, '*') &&
+           getline(file, status)) {
+
+        cout << id << "\t" << name << "\t" << author << "\t" << status << endl;
+    }
+
+    file.close();
+}
+
+void searchBook() {
+    string search, id, name, author, status;
+    bool found = false;
+
+    cout << "Enter Book ID: ";
+    getline(cin, search);
+
+    ifstream file("bookData.txt");
+
+    while (getline(file, id, '*') &&
+           getline(file, name, '*') &&
+           getline(file, author, '*') &&
+           getline(file, status)) {
+
+        if (id == search) {
+            cout << "\nBook Found:\n";
+            cout << "ID: " << id << endl;
+            cout << "Name: " << name << endl;
+            cout << "Author: " << author << endl;
+            cout << "Status: " << status << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+        cout << "Book not found!\n";
+
+    file.close();
 }
